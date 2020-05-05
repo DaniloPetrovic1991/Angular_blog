02f4444f28301e2dafcdac542c9d529ff0231317
Danilo Petrovic <dpetrovicdis@gmail.com>
2020-05-05 16:56:33 +0200

    Added styles and mock articles

---
+++ src/app/mock-articles.ts
@@ -0,0 +1,22 @@
+import { Article } from './article';
+
+export const ARTICLES: Article[] = [
+  {
+    id: 1,
+    title: 'Acoustic guitar',
+    key: 'acoustic-guitar',
+    date: new Date(),
+    content: '',
+    description: 'This is Acoustic guitar',
+    imageUrl: 'assets/images/guitar.png'
+  },
+  {
+    id: 2,
+    title: 'Drums',
+    key: 'drums',
+    date: new Date(),
+    content: '',
+    description: 'My new drums',
+    imageUrl: 'assets/images/drums.png'
+  },
+];
